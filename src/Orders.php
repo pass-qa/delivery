15cdecd4f70d5a5fce128611cbf5db9936f81e66
Mostafa sharami <mostafa.sharami@gmail.com>
2023-02-26 14:37:09 +0330

    feat (src/Orders.php): create __construct method. get 'token' and create $options default value.

--- src/Orders.php
+++ src/Orders.php
@@ -5,4 +5,22 @@ namespace PassQa\Delivery;
 class Orders
 {
     private array $options;
+
+    public function __construct($token)
+    {
+        $this->options = [
+            CURLOPT_URL            => "https://api.pass.qa/business/v1/orders",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => "",
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "GET",
+            CURLOPT_HTTPHEADER     => [
+                "Authorization: Bearer {$token}",
+                'Content-Type: application/json; charset=utf-8',
+                'Accept: application/json'
+            ],
+        ];
+    }
 }
