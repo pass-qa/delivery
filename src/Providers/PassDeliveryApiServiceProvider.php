0733488ca09f23fd6f15aff3fd02f20a3d1c7acf
Mostafa sharami <mostafa.sharami@gmail.com>
2023-02-26 14:45:31 +0330

    feat (src/Providers/PassDeliveryApiServiceProvider.php): create service provider.

---
+++ src/Providers/PassDeliveryApiServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PassQa\Delivery\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use PassQa\Delivery\Orders;
+
+class PassDeliveryApiServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
