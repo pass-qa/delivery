e559dfc9f18020d85c7b9ae443a9fe858115ef37
Majva <m.vahidkhoo@gmail.com>
2023-03-04 12:05:24 +0330

    feat(test): create testCancel method

--- library/tests/PassDeliveryOrderTest.php
+++ library/tests/PassDeliveryOrderTest.php
@@ -39,42 +39,58 @@ class PassDeliveryOrderTest extends PassTestCase
         $this->assertTrue($orderDetails['status'] === "success");
     }
 
+    public function testTracking()
+    {
+        $order = new PassOrder();
+        $createdOrder = $order->create($this->prepareCreateOrderData());
+        $orderTracking = $order->tracking($createdOrder['data']['order_id']);
+        $this->assertTrue($orderTracking['status'] === "success");
+    }
+
+    public function testCancel()
+    {
+        $order = new PassOrder();
+        $createdOrder = $order->create($this->prepareCreateOrderData());
+        $canceledResponse = $order->cancel($createdOrder['data']['order_id']);
+        $this->assertTrue($canceledResponse['status'] === "success");
+    }
+
     private function prepareCreateOrderData(): array
     {
         return [
-            "addresses"=> [
-                "pickup" =>[
-                    "lat"=> "25.275047",
-                    "long"=> "51.535141",
-                    "name"=> "majva",
-                    "phone"=> "+97466661234",
-                    "address"=> "this street",
-                    "description"=> "it is a sample description"
+            "addresses" => [
+                "pickup"   => [
+                    "lat"         => "25.275047",
+                    "long"        => "51.535141",
+                    "name"        => "majva",
+                    "phone"       => "+97466661234",
+                    "address"     => "this street",
+                    "description" => "it is a sample description"
                 ],
                 "dropoffs" => [
                     [
-                        "lat"=> "25.277007",
-                        "long"=> "51.530034",
-                        "name"=> "majva",
-                        "phone"=> "+97466661234",
-                        "address"=> "that street",
-                        "description"=> "it is a sample description"
+                        "lat"         => "25.277007",
+                        "long"        => "51.530034",
+                        "name"        => "majva",
+                        "phone"       => "+97466661234",
+                        "address"     => "that street",
+                        "description" => "it is a sample description"
                     ],
                     [
-                        "lat"=> "25.277007",
-                        "long"=> "51.530034",
-                        "name"=> "majva",
-                        "phone"=> "+97466661234",
-                        "address"=> "other street",
-                        "description"=> "it is a sample description"
+                        "lat"         => "25.277007",
+                        "long"        => "51.530034",
+                        "name"        => "majva",
+                        "phone"       => "+97466661234",
+                        "address"     => "other street",
+                        "description" => "it is a sample description"
                     ],
                     [
-                        "lat"=> "25.277007",
-                        "long"=> "51.530034",
-                        "name"=> "majva",
-                        "phone"=> "+97466661234",
-                        "address"=> "another street",
-                        "description"=> "it is a sample description"
+                        "lat"         => "25.277007",
+                        "long"        => "51.530034",
+                        "name"        => "majva",
+                        "phone"       => "+97466661234",
+                        "address"     => "another street",
+                        "description" => "it is a sample description"
                     ]
                 ]
             ]
@@ -84,21 +100,21 @@ class PassDeliveryOrderTest extends PassTestCase
     private function preparePriceData()
     {
         return [
-            "pickup" =>[
-                "lat" =>"25.275047",
+            "pickup"   => [
+                "lat"  => "25.275047",
                 "long" => "51.535141"
             ],
             "dropoffs" => [
                 [
-                    "lat" =>"25.277007",
+                    "lat"  => "25.277007",
                     "long" => "51.530034"
                 ],
                 [
-                    "lat" =>"25.277005",
+                    "lat"  => "25.277005",
                     "long" => "51.530039"
                 ],
                 [
-                    "lat" =>"25.277001",
+                    "lat"  => "25.277001",
                     "long" => "51.530030"
                 ]
             ]
